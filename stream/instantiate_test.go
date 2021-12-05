a763b1e0e57ee733d97c62f7c95fb54b644bc329
Mario Macias <mmaciasl@redhat.com>
2021-12-05 17:24:28 +0100

    test instantiate and generate

--- stream/instantiate_test.go
+++ stream/instantiate_test.go
@@ -7,5 +7,25 @@ import (
 )
 
 func TestIterate(t *testing.T) {
-	assert.Fail(t, "TO DO")
+	gen := Iterate(2, func(n int) int {
+		return n * n
+	})
+	assert.Equal(t,
+		[]int{2, 4, 16, 256, 65536},
+		gen.Limit(5).ToSlice())
+	// test that iterating for the second time produces the same results
+	assert.Equal(t,
+		[]int{2, 4, 16, 256, 65536},
+		gen.Limit(5).ToSlice())
+}
+
+func TestGenerate(t *testing.T) {
+	cnt := 0
+	gen := Generate(func() int {
+		cnt++
+		return cnt
+	})
+	assert.Equal(t,
+		[]int{1, 2, 3, 4, 5},
+		gen.Limit(5).ToSlice())
 }
